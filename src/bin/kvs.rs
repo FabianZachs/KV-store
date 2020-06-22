46cd38a27276a7cf7359ef4a2120783435b2fbee
Fabian Zachs <fabianzachs@Fabians-MacBook-Pro.local>
2020-06-22 14:25:01 +0100

    Complete Project 1: Part 2

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,4 +1,48 @@
-fn main() {
-    println!("hello");
-}
+#[macro_use]
+extern crate clap;
+use clap::{App, AppSettings, Arg, SubCommand};
+use std::process::exit;
 
+fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .arg(Arg::with_name("KEY").help("Key").required(true))
+                .arg(Arg::with_name("VALUE").help("value").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get the string value of a given string key")
+                .arg(Arg::with_name("KEY").help("key").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("Remove a given key")
+                .arg(Arg::with_name("KEY").help("key").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", Some(args)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("get", Some(args)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(args)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => {
+            eprintln!("Unknown command");
+            exit(1);
+        }
+    }
+}
