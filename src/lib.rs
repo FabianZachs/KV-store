ffff1d7fb1708fd746ecd2e7e37aba24b31c0dc4
Fabian Zachs <fabianzachs@Fabians-MacBook-Pro.local>
2020-06-24 10:56:18 +0100

    add implementation and documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,33 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+/// The `KvStore` stores String key/value pairs.
+/// This is not disk persistant.
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
 
 impl KvStore {
+    /// Creates a new `KvStore`.
     pub fn new() -> KvStore {
-        panic!()
+        KvStore {
+            map: HashMap::new(),
+        }
     }
 
+    /// Sets a String key to a String value.
     pub fn set(&mut self, k: String, v: String) {
-        panic!()
+        self.map.insert(k, v);
     }
 
+    /// Returns the String value corresponding to a key.
+    ///
+    /// Returns `None` if no such key has been  stored.
     pub fn get(&self, k: String) -> Option<String> {
-        panic!()
+        Some(String::from(self.map.get(&k)?))
     }
 
+    /// Remmoves a key-value entry via the key
     pub fn remove(&mut self, k: String) {
-        panic!()
+        self.map.remove(&k);
     }
 }
